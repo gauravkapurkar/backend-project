2840878cb731942c9e9f8689b94a1e18ddf40d6a
gauravkapurkar <gauravkapurkar@gmail.com>
2023-12-31 23:25:35 +0530

    add cloudinary & multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { v2 as cloudinary} from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        const uploadResponse = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto",
+        })
+        console.log(`Cloudinary ${uploadResponse.url} upload completed`);
+        return uploadResponse;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove locally saved files in error case
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
