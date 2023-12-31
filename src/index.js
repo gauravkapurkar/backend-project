91b41b154508e14fd3b3921235e7b2cc1e07c9c0
gauravkapurkar <gauravkapurkar@gmail.com>
2023-12-31 11:56:41 +0530

    standardize error, response

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+
+    app.on("error", (error) => {
+        console.log(error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`app listening on ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongo db connection failed ", err);
+})
 
 /*import express from "express";
 const app = express()
@@ -14,7 +29,7 @@ const app = express()
 ;( async() => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", () => {
+        app.on("error", (error) => {
             console.log(error);
             throw error;
         })
